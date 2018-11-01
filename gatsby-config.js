209901ab169704d8277d624f10617ffba3fa2b2d
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2018-11-01 10:48:34 -0700

    feat: add gatsby-plugin-google-analytics (#31)

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,15 @@ module.exports = {
       }
     },
     'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `${process.env.TRACKING_ID}`,
+        head: false,
+        anonymize: true,
+        respectDNT: true
+      },
+    },
     'gatsby-plugin-offline',
     {
       resolve: 'gatsby-plugin-manifest',
