4eb422dc66ccc972995aa5d685fd8f5107ddbc5c
Sebastián Veggiani <sveggiani@gmail.com>
2019-04-30 16:48:15 -0300

    Config moved to theme project (#60)
    * Config moved to theme project
    
    * Update gatsby version

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,31 +8,5 @@ module.exports = {
   siteMetadata: {
     title: `${process.env.SITE_NAME}`,
     domain: `${process.env.PROJECT_URL}`
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          '@weknow/gatsby-remark-drupal',
-          '@weknow/gatsby-remark-twitter',
-          'gatsby-remark-embed-video',
-          'gatsby-remark-responsive-iframe',
-          {
-            resolve: 'gatsby-remark-external-links',
-            options: {
-              target: '_blank',
-              rel: 'nofollow'
-            }
-          },
-          {
-            resolve: 'gatsby-remark-embed-gist',
-            options: {
-              includeDefaultCss: true
-            }
-          }
-        ]
-      }
-    }
-  ]
+  }
 };
