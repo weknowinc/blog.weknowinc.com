a9e711939604c7364344677e6845cb042559cd2a
Sebastián Veggiani <sveggiani@gmail.com>
2019-04-30 17:57:17 -0300

    Site info config removed (#61)

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,5 @@ module.exports = {
       resolve: '@weknow/gatsby-theme-drupal-boina',
       options: { root: __dirname }
     }
-  ],
-  siteMetadata: {
-    title: `${process.env.SITE_NAME}`,
-    domain: `${process.env.PROJECT_URL}`
-  }
+  ]
 };
